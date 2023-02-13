a39b5bd7830f83fd76346626aba58f0cf6983729
fercguerra <fercguerra06@gmail.com>
2023-02-13 08:33:36 -0300

    classe

--- poo/classe_data.dart
+++ poo/classe_data.dart
@@ -3,6 +3,14 @@ class Data {
     late int mes;
     late int ano;
 
+    String obterFormatada() {
+     return '$dia/$mes/$ano';
+    }
+
+    String toString() {
+      return obterFormatada();
+    }
+
 }
 void main() {
    var dataAnversario = new Data();
@@ -14,10 +22,15 @@ void main() {
    dataCompra.dia = 23;
    dataCompra.mes = 12;
    dataCompra.ano = 2022;
-
-
    
-print("${dataAnversario.dia}/${dataAnversario.mes}/${dataAnversario.ano}");
-print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+//print("${dataAnversario.dia}/${dataAnversario.mes}/${dataAnversario.ano}");
+//print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+String d1 = dataAnversario.obterFormatada();
+String d2 = dataCompra.obterFormatada();
+
+print("A data do aniversário é ${dataAnversario.obterFormatada()}");
+print("A data do aniversário é $d2");
+
 }
+
   
